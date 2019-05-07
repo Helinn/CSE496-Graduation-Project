12a759432170b67bb900b2343c1119dcf05d39fe
Helinn <helinyildirim@gtu.edu.tr>
2019-05-07 21:52:06 +0300

    construct real graph
    read data from mysql and construct real graph with it. Generate embeddings and TSNE formats with constructed graph

--- Tweet.py
+++ Tweet.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 class Tweet:
     num = 0
-    def __init__(self, user, text, date, *args):
+    def __init__(self,Id, user, text, date, *args):
         self.user = user
         self.text = text
         self.date = date
         self.data = args
-        self.num = Tweet.num
-        Tweet.num += 1
+        self.Id = Id
     def __repr__(self):
-        return ' '.join([self.user,self.text,self.date.strftime("%d.%m.%Y"),str(self.num)])
-
-
-# In[ ]:
-
+        return ' '.join([self.Id,self.user,self.text,self.date.strftime("%d.%m.%Y")])
 
 
 
